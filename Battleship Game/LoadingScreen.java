8429f94adc9e1a7c4b71405e25c7a6c6969e92bf
danwei002 <35182369+danwei002@users.noreply.github.com>
2020-06-10 14:05:17 -0400

    Added loading frames

---
+++ Battleship Game/LoadingScreen.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class LoadingScreen here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LoadingScreen extends World
+{
+    private GreenfootImage[] frames ;
+    
+    /**
+     * Constructor for objects of class LoadingScreen.
+     * 
+     */
+    public LoadingScreen()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
